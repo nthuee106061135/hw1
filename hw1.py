673e5341fef57dfd696f08d1e39f44bda0c8f426
nthuee106061135 <frank880415@gmail.com>
2020-03-17 21:27:06 +0800

    hw1.2nd

--- hw1.py
+++ hw1.py
@@ -52,33 +52,33 @@ value4 = []
 
 for i in range(len(target_data0)):
    value0.append(i)
-   if((target_data0[i]['WDSD'] is '-99.000') | (target_data0[i]['WDSD'] is '-999.000')):
-      value0[i] = 0.5
+   if((target_data0[i]['WDSD'] == '-99.000') | (target_data0[i]['WDSD'] == '-999.000')):
+      value0[i] = 0.499
    else:
       value0[i] = target_data0[i]['WDSD']
 for i in range(len(target_data1)):
    value1.append(i)
-   if((target_data1[i]['WDSD'] is '-99.000') | (target_data1[i]['WDSD'] is '-999.000')):
-      value1[i] = 0.5
+   if((target_data1[i]['WDSD'] == '-99.000') | (target_data1[i]['WDSD'] == '-999.000')):
+      value1[i] = 0.499
    else:
       value1[i] = target_data1[i]['WDSD']
 for i in range(len(target_data2)):
    value2.append(i)
-   if((target_data2[i]['WDSD'] is '-99.000') | (target_data2[i]['WDSD'] is '-999.000')):
-      value2[i] = 0.5
+   if((target_data2[i]['WDSD'] == '-99.000') | (target_data2[i]['WDSD'] == '-999.000')):
+      value2[i] = 0.499
    else:
       value2[i] = target_data2[i]['WDSD']
 
 for i in range(len(target_data3)):
    value3.append(i)
-   if((target_data3[i]['WDSD'] is '-99.000') | (target_data3[i]['WDSD'] is '-999.000')):
-      value3[i] = 0.5
+   if((target_data3[i]['WDSD'] == '-99.000') | (target_data3[i]['WDSD'] == '-999.000')):
+      value3[i] = 0.499
    else:
-      value3[i] = target_data3[i]['WDSD']
+      value3[i] = float(target_data3[i]['WDSD'])
 for i in range(len(target_data4)):
    value4.append(i)
-   if((target_data4[i]['WDSD'] is '-99.000') | (target_data4[i]['WDSD'] is '-999.000')):
-      value4[i] = 0.5
+   if((target_data4[i]['WDSD'] == '-99.000') | (target_data4[i]['WDSD'] == '-999.000')):
+      value4[i] = 0.499
    else:
       value4[i] = target_data4[i]['WDSD'] 
 
@@ -98,6 +98,40 @@ x1 = float(max_value1) - float(min_value1)
 x2 = float(max_value2) - float(min_value2)
 x3 = float(max_value3) - float(min_value3)
 x4 = float(max_value4) - float(min_value4)
+if( x0 == 0) :
+   x0 = 'None'
+elif (max_value0 == 0.499):
+   x0 = 'None'
+elif (min_value0 == 0.499):
+   x0 = 'None' 
+
+if( x1 == 0):
+   x1 = 'None'
+elif (max_value1 == 0.499):
+   x1 = 'None'
+elif (min_value1 == 0.499):
+   x1 = 'None'
+
+if( x2 == 0):
+   x2 = 'None'
+elif (max_value2 == 0.499):
+   x2 = 'None'
+elif (min_value2 == 0.499):
+   x2 = 'None'
+
+if( x3 == 0):
+   x3 = 'None'
+elif (max_value3 == 0.499):
+   x3 = 'None'
+elif (min_value3 == 0.499):
+   x3 = 'None'
+
+if( x4 == 0):
+   x4 = 'None'  
+elif (max_value4 == 0.499):
+   x4 = 'None'
+elif (min_value4 == 0.499):
+   x4 = 'None'
 
 print('[[',target_data0[0]['station_id'],',',x0,']',
       '[',target_data1[0]['station_id'],',',x1,']',
